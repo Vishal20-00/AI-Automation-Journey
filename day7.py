156d5d23f1273afa8e35e7b304181c6765a39ef2
Vishal20-00 <v.verma15637@gmail.com>
2026-07-19 09:15:51 +0530

    Create day7.py

---
+++ day7.py
@@ -0,0 +1,16 @@
+task = "done"
+task_1 = input("Have you completed the study of Python? (done/not done):").lower()
+while task_1 != task:
+    print("You haven't done your task Sir, Do your task first and if done write done")
+    task_1 = input("Have you completed the study of Python? (done/not done):").lower()
+print("Congratulation on completion of task 1")
+task_2 = input("Have you done running? (done/not done):").lower()
+while task_2 != task:
+    print("You haven't done your task Sir, Do your task first and if done write done")
+    task_2 = input("Have you done running? (done/not done):").lower()
+print("Congratulation on completion of second task")
+task_3 = input("Have you done reading books?: (done/not done)")
+while task_3 != task:
+    print("You haven't completed your task, do your task first")
+    task_3 = input("Have you done your third task?: (done/not done)")
+print("Congratulations on the completion of third task.")
