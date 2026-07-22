4ea0ecb02d714b49ab2a432e80cbc20df462f072
Vishal20-00 <v.verma15637@gmail.com>
2026-07-22 12:33:45 +0530

    Create day8.py

---
+++ day8.py
@@ -0,0 +1,5 @@
+tasks = ["5 AM Club", "Pranayam", "Study", "Breakfast", "Study", "AI_Automation", "Running", "Exercising", "Dinner", "Duolingo","Sleep"]
+for task in tasks:
+    print("My today's task is", task)
+    if task == "AI_Automation":
+        print("Python is my AI_Automaton task")
