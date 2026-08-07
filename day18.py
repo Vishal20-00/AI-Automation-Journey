0dcaa9d6bbd28093f712512405949ead5407d3e4
Vishal20-00 <v.verma15637@gmail.com>
2026-08-07 19:56:27 +0530

    Create day18.py

---
+++ day18.py
@@ -0,0 +1,18 @@
+completed = []
+print("Starting AI Assistant......")
+
+completed . append("Read PDF") #it adds the item in the list
+completed . append("Extract Text")
+completed . append("Summarize")
+completed . append("Save Report")
+print(completed)
+print(len(completed))
+
+completed . remove("Read PDF") #it removes the item by the name we tell it to remove.
+completed . remove("Extract Text")
+print(completed)
+print(len(completed))
+
+fruits = ["Apple", "Mango", "Banana", "Orange", "Guava", "Blue Barry"]
+fruits.pop(2) #it removes the item according to the given number, here index value starts from 0
+print(fruits)
